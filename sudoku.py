5d4ab1ccb8898c85936a3188c24031fd47af9071
Mikuláš Zelinka <mikulas@zelinka.dev>
2021-01-15 00:15:12 +0100

    feat: working numpy solver (first try :kreygasm: :weirdflexbutok:)

---
+++ sudoku.py
@@ -0,0 +1,84 @@
+# From: https://qqwing.com/
+# Input sample:
+# ......4....69....3..2.4..6.4.7.98.....82.3..4.2..7.1....97...3.......2.78..3.2.9.
+#
+# Solution:
+#
+#  9 8 1 | 5 3 6 | 4 7 2
+#  5 4 6 | 9 2 7 | 8 1 3
+#  7 3 2 | 8 4 1 | 9 6 5
+# -------|-------|-------
+#  4 5 7 | 1 9 8 | 3 2 6
+#  1 9 8 | 2 6 3 | 7 5 4
+#  6 2 3 | 4 7 5 | 1 8 9
+# -------|-------|-------
+#  2 6 9 | 7 1 4 | 5 3 8
+#  3 1 5 | 6 8 9 | 2 4 7
+#  8 7 4 | 3 5 2 | 6 9 1
+#
+# Number of Givens: 27
+# Number of Singles: 73
+# Number of Hidden Singles: 22
+# Number of Naked Pairs: 2
+# Number of Hidden Pairs: 0
+# Number of Pointing Pairs/Triples: 3
+# Number of Box/Line Intersections: 1
+# Number of Guesses: 8
+# Number of Backtracks: 8
+# Difficulty: Expert
+
+import numpy as np
+from numba import njit
+
+
+@njit
+def index_of(array, item):
+    for idx, val in np.ndenumerate(array):
+        if val == item:
+            yield idx
+
+
+def to_numpy(sudoku_text):
+    """
+    sudoku_text expects 81 chars
+    "Output format" to "One line", e.g.:
+      ......4....69....3..2.4..6.4.7.98.....82.3..4.2..7.1....97...3.......2.78..3.2.9.
+    """
+    return np.array([int(x) for x in sudoku_text.replace('.', '0')], dtype=np.uint8).reshape((9, 9))
+
+
+all_numbers = np.arange(1, 10, 1, dtype=np.uint8)
+
+
+def solve(sudoku):
+    if np.all(sudoku):
+        print('done')
+        print(sudoku)
+        return
+
+    x, y = next(index_of(sudoku, 0))
+    x_left = 0 if x <= 2 else 3 if x <= 5 else 6
+    y_left = 0 if y <= 2 else 3 if y <= 5 else 6
+    forbidden_numbers = np.unique(
+        np.concatenate((
+            sudoku[:, y],
+            sudoku[x, :],
+            sudoku[x_left:x_left + 3, y_left:y_left + 3].ravel()
+        ))
+    )
+    candidates = np.setdiff1d(all_numbers, forbidden_numbers)
+
+    if len(candidates) == 0:
+        return
+
+    for c in candidates:
+        sudoku[x, y] = c
+        solve(sudoku)
+        sudoku[x, y] = 0
+
+
+sudoku_text = '......4....69....3..2.4..6.4.7.98.....82.3..4.2..7.1....97...3.......2.78..3.2.9.'
+sudoku_array = to_numpy(sudoku_text)
+
+print(f'attempting to solve\n{sudoku_array}')
+solve(sudoku_array)
